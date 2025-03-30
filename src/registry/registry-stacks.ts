45d097a0a13141e705536f185c5e61097f76d8da
matheussatoshi1 <matheussdev3@gmail.com>
2025-03-30 04:12:35 -0400

    feat(constants): adds experiences, social-medias, stacks and works mockup data

---
+++ src/registry/registry-stacks.ts
@@ -0,0 +1,26 @@
+export const STACK = [
+  {
+    id: 1,
+    src: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+    alt: "React logo",
+    label: "React",
+  },
+  {
+    id: 2,
+    src: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+    alt: "React Native logo",
+    label: "React Native",
+  },
+  {
+    id: 3,
+    src: "https://img.icons8.com/fluent-systems-filled/200/FFFFFF/nextjs.png",
+    alt: "Next.js logo",
+    label: "Next.js",
+  },
+  {
+    id: 4,
+    src: "https://static-00.iconduck.com/assets.00/node-js-icon-1817x2048-g8tzf91e.png",
+    alt: "Node.js logo",
+    label: "Node.js",
+  },
+];
---
+++ src/registry/registry-works.ts
@@ -0,0 +1,16 @@
+export const ACTUALLY_WORKS = [
+  {
+    id: 1,
+    label: "Ream",
+    href: "",
+    src: "https://ream.com.br/wp-content/uploads/2022/09/logoream.png",
+    alt: "Ream logo brand.",
+  },
+  {
+    id: 2,
+    label: "Atem",
+    href: "",
+    src: "https://atem.com.br/wp-content/themes/conatus-atem/img/logo/Logo-Atem-PNG.png",
+    alt: "Atem Group logo brand.",
+  },
+];
