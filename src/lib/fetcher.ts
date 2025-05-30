31449be41741fbaf37fb58d8b6ef40cb07ef6129
matheussatoshi1 <matheussdev3@gmail.com>
2025-05-30 18:46:07 -0400

    refactor: improve URL construction in fetcher function to handle server and client environments, ensuring proper API URL usage

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,8 +1,16 @@
+const isServer = typeof window === "undefined";
+
 export async function fetcher<T = unknown>(
   input: RequestInfo | URL,
   init?: RequestInit,
 ): Promise<T> {
-  const url = `${process.env.NEXT_PUBLIC_API_URL}${input}`;
+  const baseURL = isServer
+    ? process.env.NEXT_PUBLIC_API_URL?.startsWith("http")
+      ? process.env.NEXT_PUBLIC_API_URL
+      : `https://www.matheussfigueiredo.com/api`
+    : process.env.NEXT_PUBLIC_API_URL;
+
+  const url = `${baseURL}${input}`;
 
   const response = await fetch(url, init);
 
