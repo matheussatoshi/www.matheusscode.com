67909887916154b6bca7f87d257902b0582dc3ea
matheussatoshi1 <matheussdev3@gmail.com>
2025-05-30 18:30:30 -0400

    refactor: enhance URL handling in fetcher function to support full URLs and improve clarity in request construction

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -2,10 +2,15 @@ export async function fetcher<T = unknown>(
   input: RequestInfo | URL,
   init?: RequestInit,
 ): Promise<T> {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api${input}`,
-    init,
-  );
+const isFullUrl = typeof input === "string" && input.startsWith("http");
+
+  const url = isFullUrl
+    ? input
+    : typeof window === "undefined"
+      ? `/api${input}`
+      : `${process.env.NEXT_PUBLIC_API_URL}/api${input}`;
+
+  const response = await fetch(url, init);
 
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
