7bfc1927f1a8b89bc626fb9cbfaad0e48431e307
matheussatoshi1 <matheussdev3@gmail.com>
2025-06-22 20:00:47 -0400

    feat: adds vercel analytics and speed-insights for performance tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { ThemeProvider } from "@/providers/theme-provider";
 import { AppNavBar } from "@/ui/layout/app-nav-bar";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 
@@ -16,6 +18,8 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
           <AppNavBar />
           {children}
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
